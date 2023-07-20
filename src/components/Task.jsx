242b656076efd08dadc22707aa1f5e7ad2f1b491
PremerX007 <39229888+PremerX007@users.noreply.github.com>
2023-07-21 03:22:25 +0700

    completed

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -9,6 +9,12 @@ export const Task = ({
     deleteTaskFunc(id);
   };
 
+  const doneTaskFuncOnClick = () => {
+    toggleDoneTaskFunc(id);
+  };
+
+  const resultClassname = completed ? "text-decoration-line-through" : "";
+
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
       {/*
@@ -17,8 +23,10 @@ export const Task = ({
         But if task is not completed : 
         <span>{title}</span>
       */}
-      <span>{title}</span>
-      <button className="btn btn-success">Done</button>
+      <span className={resultClassname}>{title}</span>
+      <button className="btn btn-success" onClick={doneTaskFuncOnClick}>
+        Done
+      </button>
       <button className="btn btn-danger" onClick={deleteBtnOnClick}>
         Delete
       </button>
